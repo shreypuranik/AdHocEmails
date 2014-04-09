5618ca62f65046c19e4a5ad1297292ab0d23ecee
shreypuranik <code@shreypuranik.co.uk>
2014-04-09 21:59:12 +0100

    Adding in Logger class
    Adding in first version of logger class, and updated README.
    
    Please download the PEAR DB files from the PEAR website, and store in
    the same directory on your web server. Also create a simple table,
    EmailLogs, with 2 columns (useremail, emailtitle) and connect
    appropriately in the setDB method.
    
    More updates to follow

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 AdHocEmails
 ===========
 
-This download contains 1 PHP class. 
+Last updated 9th April 2014
 
-Using the EmailSender class 
+This download contains 2 PHP classes. 
+
+SIMPLE USE: Using the EmailSender class 
 
 Instantiate as below: 
 
@@ -16,3 +18,21 @@ Add as many email addresses as you need using the add method.
 Send the email by calling $obj->sendMailing(). 
 
 No sanitisation methods used (at present).  
+
+ADVANCED USE (Currently a Work in Progress)
+
+Using the EmailSenderLogger class 
+
+Instantiate as below: 
+
+$emailer = new EmailSenderLogger(); 
+
+Set methods as outlined above 
+
+When sending the emails, this class will log the email recipient email address, and the email title. 
+
+Database schema to be committed in shortly. 
+
+
+
+
---
+++ class.EmailSenderLogger.php
@@ -0,0 +1,52 @@
+<?
+include_once("class.EmailSender.php");
+include_once("DB.php"); 
+
+/* Logger class, which builds upon the basic 
+ * functionality provided by the main 
+ * EmailSender class. Logs email sending 
+ * and times. 
+ *
+ * Uses the PEAR DB files to connect to 
+ * MYSQL. Please download this separately
+ */
+
+class EmailSenderLogger extends EmailSender{
+
+	function __construct(){
+		parent::__construct(); 
+        $this->setDB(); 
+
+	}
+
+	function setDB(){
+		$this->db = DB::connect('mysql://'user':'password'@'host'/'database_name'');
+	}
+
+	function logEmail($email, $title){
+		$this->db->query("INSERT INTO `EmailLogs` (`useremail`, `emailtitle`) VALUES (?, ?)", array($email, $title));
+	}
+
+
+
+	/* Send the mailing to the email 
+	 * addresses in the distribution 
+	 * list. 
+	 */ 
+	function sendMailing(){
+		if ((count($this->emailUsers)>0) && $this->emailSubject && $this->emailBody){
+			if ($this->addTime){
+				//append timestamp logic to the email 
+				$additionalString = "\n"; 
+				$additionalString .="This emai was sent on ".date('l jS Y', $this->timestamp);
+				$this->emailBody .= $additionalString;
+			}
+
+		foreach($this->emailUsers as $emailUser){		
+			mail($emailUser, $this->emailSubject, $this->emailBody); 
+			$this->logEmail($emailUser, $this->emailSubject);
+		}
+	}
+
+	}
+}
