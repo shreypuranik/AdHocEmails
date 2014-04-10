cad31cec6d53934e1dcfadc078257f595821b408
shreypuranik <code@shreypuranik.co.uk>
2014-04-10 22:13:11 +0100

    Manager type class added
    Adding in basic foundations of ‘Manager’ class to allow basic stats to
    be returned at ‘Admin’ level.

---
+++ class.EmailSenderManager.php
@@ -0,0 +1,82 @@
+<?
+include_once("DB.php"); 
+
+
+
+class EmailSenderManager{
+
+	private $db; 
+	private $queryMailTitle; 
+	private $queryMailUserEmail; 
+
+
+	function __construct(){
+		$this->setDB(); //set the database 
+	}
+
+
+
+	/* Connect to the database */ 
+	function setDB(){
+		$this->db = DB::connect('mysql://'user':'password'@'host'/'database_name'');
+	}
+
+	
+	/* Set the title of the suggested email */ 
+	function setQueryMailTitle($data){
+		$this->queryMailTitle = $data; 
+	}
+
+	/* Return the title of the suggested email 
+	 * @return string 
+	 */ 
+	function getQueryMailTitle(){
+		return $this->queryMailTitle; 
+	}
+
+	/* Set the user email you want to query */ 
+	function setQueryMailUserEmail($data){
+		$this->queryMailUserEmail = $data; 
+	}
+
+
+	/* Get the user email for the sole query 
+	 * @return string 
+	 */ 
+	function getQueryMailUserEmail(){
+		return $this->queryMailUserEmail; 
+	}
+
+
+	/* Return data for sole user query 
+	 * against set title 
+	 * @return array 
+	 */ 
+	function getSoleUserQuery(){
+		$data = $this->db->getAll("SELECT * FROM `EmailLogs` WHERE `useremail` = ? AND emailtitle = ?", array($this->queryMailUserEmail, $this->queryMailTitle)); 
+		return $data; 
+	}
+
+	/* Returns data for sole title query 
+	 * @return array 
+	 */ 
+	function getSoleTitleQuery(){
+		$data = $this->db->getAll("SELECT `useremail` FROM `EmailLogs` WHERE `emailtitle` = ?", array($this->queryMailTitle)); 
+		return $data; 
+	}
+
+
+
+
+}
+
+$manager = new EmailSenderManager(); 
+$manager->setQueryMailUserEmail('shrey@shreypuranik.co.uk');
+$manager->setQueryMailTitle('iTunes Top 10'); 
+$results = $manager->getSoleTitleQuery(); 
+var_dump($results); 
+
+
+
+
+
